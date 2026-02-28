1bfa631e64b0997fe275200173c900ea68487702
Lin <caiqinghua@126.com>
2026-02-28 23:08:14 +0800

    fix pm2 restart because of logs file change

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
     script: "pnpm",
     args: "start -p 3000 -H 0.0.0.0",
     watch: '.',
+    ignore_watch: ['logs', 'node_modules', '.git', '.next', '.claude'],
     cwd: '.',
   }],
 };
